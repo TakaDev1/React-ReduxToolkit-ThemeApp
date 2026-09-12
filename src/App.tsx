195f8b71f05ae3decc808f81235e30fe32ad1802
Izumi Takayuki <r8quattrospider@gmail.com>
2026-09-12 16:48:09 +0900

    feat: Implement DisplayTheme

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { Provider } from "react-redux";
 import "./App.css";
 import store from "./app/store";
 import ThemeToggle from "./features/theme/components/ThemeToggle";
+import DisplayTheme from "./features/theme/components/DisplayTheme";
 
 function App() {
   return (
     <>
       <Provider store={store}>
+        <DisplayTheme />
         <ThemeToggle />
       </Provider>
     </>
